cc15508bca73328cb7663bff2eccd611a087c34b
bpliew <arielcarino3@gmail.com>
2016-10-01 15:08:28 +0800

    Add files via upload

---
+++ 1000Enchants.js
@@ -0,0 +1,148 @@
+//1000Enchants Mod
+//Description: Enchant any weapons and armors in 1000!
+//Author: bpliew
+//Facebook: https://facebook.com/xxmlgilluminatixx
+//Version: 1.0
+//This mod only works on creative mode.
+//Special thanks to ancernarry.
+//Copyright 2016
+//Don't you dare copy the source code and rewrite it.
+
+/*Changelogs:
+*v1.0
+*-Public Release
+*-Exclusive for Endermix
+*-Supports 0.15.9
+*/
+
+print("1000Enchants is Running!")
+
+function newLevel() {
+    clientMessage("§a~~~~~1000Enchants~~~~~ made by bpliew!");
+	clientMessage("§av1.0 visit fb.com/xxmlgilluminatixx for help");
+}
+
+function useItem(x,y,z,itemId,blockId)  
+
+{
+	
+if(blockId==116)
+
+print("Make sure you are on Creative mode in order to make the 1000Enchants work!")
+
+{
+
+if(blockId==116&&itemId==276)
+
+clientMessage("§6§l1000Enchants: §r§aDiamond Sword Enchanted!")
+
+{
+
+if(blockId==116&&itemId==278)
+
+clientMessage("§6§l1000Enchants: §r§aDiamond Pickaxe Enchanted!")
+
+{
+
+if(blockId==116&&itemId==279)
+
+clientMessage("§6§l1000Enchants: §r§aDiamond Axe Enchanted!")
+
+
+{
+
+if(blockId==116&&itemId==310)
+
+clientMessage("§6§l1000Enchants: §r§aDiamond Helmet Enchanted!")
+
+{
+
+if(blockId==116&&itemId==311)
+
+clientMessage("§6§l1000Enchants: §r§aDiamond Chestplate Enchanted!")
+
+{
+
+if(blockId==116&&itemId==312)
+
+clientMessage("§6§l1000Enchants: §r§aDiamond Leggings Enchanted!")
+
+{
+
+if(blockId==116&&itemId==313)
+
+clientMessage("§6§l1000Enchants: §r§aDiamond Boots Enchanted!")
+
+{
+var ThisIsSharpness;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.SHARPNESS, 1000);
+
+var ThisIsSmite;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.SMITE, 1000);
+
+var ThisIsBaneOfArthropods;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.BANE_OF_ARTHROPODS,1000);
+
+var ThisIsKnockback;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.KNOCKBACK, 1000);
+
+var ThisIsFireAspect;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.FIRE_ASPECT, 1000);
+
+var ThisIsLooting;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.LOOTING, 1000);
+
+var ThisIsAquaAffinity;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.AQUA_AFFINITY, 1000);
+
+var ThisIsFeatherFalling;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.FEATHER_FALLING, 1000);
+
+var ThisIsFlame;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.FLAME, 1000);
+
+var ThisIsPower;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.POWER, 1000);
+
+var ThisIsPunch;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.PUNCH, 1000);
+
+var ThisIsProtection;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.PROTECTION, 1000);
+
+var ThisIsSilkTouch;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.SILK_TOUCH, 1000);
+
+var ThisIsFireProtection;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.FIRE_PROTECTION, 1000);
+
+var ThisIsRespiration;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.RESPIRATION, 1000);
+
+var ThisIsEfficiency;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.EFFICIENCY, 1000);
+
+var ThisIsUnbreaking;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.UNBREAKING, 1000);
+
+var ThisIsFortune;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.FORTUNE, 1000);
+
+var ThisIsInfinity;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.INFINITY, 1000);
+
+var ThisIsLure;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.LURE,1000);
+
+var ThisIsThorns;
+Player.enchant(Player.getSelectedSlotId(), Enchantment.THORNS,1000);
+
+}
+}
+}
+}
+}
+}
+}
+}
+}
